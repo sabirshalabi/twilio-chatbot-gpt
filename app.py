d64df1663b2d14b7cbb1fb725980c5beb5c35160
sabirshalabi <108598215+sabirshalabi@users.noreply.github.com>
2022-12-27 22:43:16 -0800

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, session
 from twilio.rest import Client
-from sabirsbot import ask, append_interaction_to_chat_log
+from bot import ask, append_interaction_to_chat_log
 import os
 
 app = Flask(__name__)
@@ -21,7 +21,7 @@ my_phone_number = os.getenv('my_phone_number')
 
 
 @app.route('/', methods=['POST'])
-def sabirsbot():
+def bot():
     # Get the incoming message from the request values
     incoming_msg = request.values['Body']
 
@@ -43,48 +43,6 @@ def sabirsbot():
 )
 
     return str(message.sid)
-
-
-
-# @app.route('/sabirsbot', methods=['POST'])
-# def sabirsbot():
-#     # Get the incoming message from the request values
-#     incoming_msg = request.values['Body']
-
-#     try:
-#         # Get the chat log from the session data
-#         chat_log = session.get('chat_log')
-#     except Exception as e:
-#         print(e)
-#         return "Error getting chat log"
-#     try:
-#         # Get the chatbot's response
-#         print("Getting chatbot's response")
-#         answer = ask(incoming_msg, chat_log)
-#     except Exception as e:
-#         print(e)
-#         return "Error getting chatbot's response"
-
-#     try:
-#         # Append the incoming message and chatbot's response to the chat log
-#         session['chat_log'] = append_interaction_to_chat_log(
-#             incoming_msg, answer, chat_log)
-#     except Exception as e:
-#         print(e)
-#         return "Error appending incoming message and chatbot's response to chat log"
-
-#     try:
-#         # Use Twilio to send the chatbot's response as a text message
-#         message = client.messages.create(
-#             body=answer,
-#             from_=twilio_phone_number,
-#             to=my_phone_number
-#         )
-#     except Exception as e:
-#         print(e)
-#         return "Error sending chatbot's response as text message"
-
-#     return str(message.sid)
 
 
 if __name__ == '__main__':
