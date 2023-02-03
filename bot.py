0ba0e36d11f9c11ffe093572e5f240f6dab11f1a
Sabir Shalabi <sabirshalabi@gmail.com>
2023-02-02 21:26:35 -0800

    -token limit handling

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import openai
 import os
 from db import insert_message, insert_response, get_messages, get_responses
+from scrape import extract_url, scrape_webpage, preprocess_data, generate_summary
+import re
 
 # Load the OpenAI API key from the .env file
 openai.api_key = os.getenv('openai_api_key')
@@ -25,47 +27,118 @@ And here is the newest message to the chatbot:
 """
 
 
+# def ask(question, message_id):
+#     """
+#     Send a query to the OpenAI API and return a response.
+#     """
+#     # Check if a URL is present in the message
+#     url_match = re.search(
+#         r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})",
+#         question)
+#     if url_match:
+#         # Extract the URL from the message
+#         url = url_match.group(1)
+#         # Scrape the webpage at the URL
+#         data = scrape_webpage(url)
+#         # Preprocess the data
+#         preprocessed_data = preprocess_data(data)
+#         # Generate a summary of the data
+#         summary = generate_summary(preprocessed_data)
+#         # Return the summary as the chatbot's response
+#         return summary
+#     # If the "scrape this" command is not present, use the OpenAI API to generate a response
+#     else:
+#         # Retrieve all messages and chatbot responses from the database
+#         messages = get_messages()
+#         chat_log = ""
+#         for message in messages:
+#             body = message[1]
+#             chat_log += f"\nPerson: {body}"
+#             responses = get_responses(message_id)
+#             for response in responses:
+#                 body = response[1]
+#                 chat_log += f"\nBot: {body}"
 #
-
+#         # Set the prompt for the query
+#         prompt = session_prompt
+#         # If a chat log is provided, include it in the prompt
+#         if chat_log:
+#             prompt = prompt.replace("{{177731366__rows}}", str(chat_log))
+#         # Include the incoming message in the prompt
+#         prompt = prompt.replace("{{177726073__body}}", str(question))
+#         # print(prompt)
+#         # Make the request to the GPT-3 API
+#         response = openai.Completion.create(
+#             engine="text-davinci-003",
+#             temperature=0.8,
+#             max_tokens=150,
+#             top_p=1,
+#             frequency_penalty=0,
+#             presence_penalty=0.3,
+#             prompt=prompt
+#         )
+#
+#         # Insert the chatbot's response into the "responses" table
+#         insert_response(response.choices[0].text, message_id)
+#
+#         # Return the response text
+#         return response.choices[0].text
 
 def ask(question, message_id):
     """
     Send a query to the OpenAI API and return a response.
     """
+    # Check if a URL is present in the message
+    url_match = re.search(
+        r"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})",
+        question)
+    if url_match:
+        # Extract the URL from the message
+        url = url_match.group(1)
+        # Scrape the webpage at the URL
+        data = scrape_webpage(url)
+        # Preprocess the data
+        preprocessed_data = preprocess_data(data)
+        # Generate a summary of the data
+        summary = generate_summary(preprocessed_data)
+        # Return the summary as the chatbot's response
+        return summary
+    # If the "scrape this" command is not present, use the OpenAI API to generate a response
+    else:
+        # Retrieve all messages and chatbot responses from the database
+        messages = get_messages()
+        chat_log = ""
+        for i, message in enumerate(messages[-10:]): # only retrieve the last 10 messages
+            body = message[1]
+            chat_log += f"\nPerson: {body}"
+            responses = get_responses(message_id)
+            for response in responses:
+                body = response[1]
+                chat_log += f"\nBot: {body}"
 
-    # Retrieve all messages and chatbot responses from the database
-    messages = get_messages()
-    chat_log = ""
-    for message in messages:
-        body = message[1]
-        chat_log += f"\nPerson: {body}"
-        responses = get_responses(message_id)
-        for response in responses:
-            body = response[1]
-            chat_log += f"\nBot: {body}"
+        # Set the prompt for the query
+        prompt = session_prompt
+        # If a chat log is provided, include it in the prompt
+        if chat_log:
+            prompt = prompt.replace("{{177731366__rows}}", str(chat_log))
+        # Include the incoming message in the prompt
+        prompt = prompt.replace("{{177726073__body}}", str(question))
+        # print(prompt)
+        # Make the request to the GPT-3 API
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            temperature=0.8,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.3,
+            prompt=prompt
+        )
 
-    # Set the prompt for the query
-    prompt = session_prompt
-    # If a chat log is provided, include it in the prompt
-    if chat_log:
-        prompt = prompt.replace("{{177731366__rows}}", str(chat_log))
-    # Include the incoming message in the prompt
-    prompt = prompt.replace("{{177726073__body}}", str(question))
-    print(prompt)
-    # Make the request to the GPT-3 API
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        temperature=0.8,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.3,
-        prompt=prompt
-    )
+        # Insert the chatbot's response into the "responses" table
+        insert_response(response.choices[0].text, message_id)
 
-    # Insert the chatbot's response into the "responses" table
-    insert_response(response.choices[0].text, message_id)
+        # Return the response text
+        return response.choices[0].text
 
-    # Return the response text
-    return response.choices[0].text
 
--- db.py
+++ db.py
@@ -1,10 +1,68 @@
+# import sqlite3
+# import uuid
+#
+#
+# def get_connection():
+#     """
+#     Establish a connection to the database and return a connection object.
+#     """
+#     conn = sqlite3.connect("chatbot.db")
+#     return conn
+#
+#
+# def insert_message(body, message_id):
+#     """
+#     Insert a new message into the "messages" table.
+#     """
+#     with get_connection() as conn:
+#         with conn:
+#             cursor = conn.cursor()
+#             cursor.execute("INSERT INTO messages (body, message_id) VALUES (?, ?)", (body, message_id))
+#
+#
+# def insert_response(body, message_id):
+#     """
+#     Insert a new chatbot response into the "chatbot_responses" table.
+#     :param body:
+#     :param message_id:
+#     :return:
+#     """
+#     with get_connection() as conn:
+#         cursor = conn.cursor()
+#         cursor.execute("INSERT INTO responses (body, message_id) VALUES (?, ?)", (body, message_id))
+#
+#
+# def get_messages():
+#     """
+#     Retrieve all messages from the "messages" table.
+#     """
+#     with get_connection() as conn:
+#         with conn:
+#             cursor = conn.cursor()
+#             cursor.execute("SELECT * FROM messages")
+#             return cursor.fetchall()
+#
+#
+# def get_responses(message_id):
+#     """
+#     Retrieve all responses for a given message ID.
+#     """
+#     with get_connection() as conn:
+#         with conn:
+#             cursor = conn.cursor()
+#             cursor.execute("SELECT * FROM chatbot_responses WHERE message_id=?", (message_id,))
+#             return cursor.fetchall()
+
 import sqlite3
 import uuid
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def get_connection():
+def create_connection():
     """
-    Establish a connection to the database and return a connection object.
+    Creates a connection to the database and returns a connection object.
     """
     conn = sqlite3.connect("chatbot.db")
     return conn
@@ -14,10 +72,19 @@ def insert_message(body, message_id):
     """
     Insert a new message into the "messages" table.
     """
-    with get_connection() as conn:
-        with conn:
-            cursor = conn.cursor()
-            cursor.execute("INSERT INTO messages (body, message_id) VALUES (?, ?)", (body, message_id))
+    if not body or not message_id:
+        raise ValueError("Both 'body' and 'message_id' are required fields")
+
+    conn = create_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO messages (body, message_id) VALUES (?, ?)", (body, message_id))
+        conn.commit()
+    except Exception as e:
+        logger.error(f"Error inserting message into database: {e}")
+        raise
+    finally:
+        conn.close()
 
 
 def insert_response(body, message_id):
@@ -27,28 +94,51 @@ def insert_response(body, message_id):
     :param message_id:
     :return:
     """
-    with get_connection() as conn:
+    if not body or not message_id:
+        raise ValueError("Both 'body' and 'message_id' are required fields")
+
+    conn = create_connection()
+    try:
         cursor = conn.cursor()
         cursor.execute("INSERT INTO responses (body, message_id) VALUES (?, ?)", (body, message_id))
+        conn.commit()
+    except Exception as e:
+        logger.error(f"Error inserting response into database: {e}")
+        raise
+    finally:
+        conn.close()
 
 
 def get_messages():
     """
     Retrieve all messages from the "messages" table.
     """
-    with get_connection() as conn:
-        with conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM messages")
-            return cursor.fetchall()
+    conn = create_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM messages")
+        return cursor.fetchall()
+    except Exception as e:
+        logger.error(f"Error retrieving messages from database: {e}")
+        raise
+    finally:
+        conn.close()
 
 
 def get_responses(message_id):
     """
     Retrieve all responses for a given message ID.
     """
-    with get_connection() as conn:
-        with conn:
-            cursor = conn.cursor()
-            cursor.execute("SELECT * FROM chatbot_responses WHERE message_id=?", (message_id,))
-            return cursor.fetchall()
+    if not message_id:
+        raise ValueError("'message_id' is a required field")
+
+    conn = create_connection()
+    try:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM responses WHERE message_id=?", (message_id,))
+        return cursor.fetchall()
+    except Exception as e:
+        logger.error(f"Error retrieving responses from database: {e}")
+        raise
+    finally:
+        conn.close()
